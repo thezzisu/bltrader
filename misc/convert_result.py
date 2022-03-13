a5587059201d4f81faa4bb6d5011aee580019172
GCC314 <gcc_dev@163.com>
2022-03-13 18:29:33 +0800

    cr v2

--- misc/convert_result.py
+++ misc/convert_result.py
@@ -1,18 +1,21 @@
 import struct
 
 
-FILE = "D:\\Downloads\\100x10x10\\trade1"
+FILE_prefix = "./100x10x10/trade"
+OUT_prefix = "./trade"
 
-arr = []
-
-with open(FILE, 'rb') as f:
-    while True:
-        buf = f.read(24)
-        if not buf:
-            break
-        arr.append(struct.unpack("=iiidi", buf))
-
-print(len(arr))
-for i in arr:
-    _, a, b, c, d = i
-    print(a, b, c, d)
+for i in range(1,11):
+	arr = []
+	FILE = FILE_prefix + str(i)
+	OUT = OUT_prefix + str(i)
+	with open(FILE, 'rb') as f:
+		while True:
+			buf = f.read(24)
+			if not buf:
+				break
+			arr.append(struct.unpack("=iiidi", buf))
+	with open(OUT,'w') as f:
+		print(len(arr),file = f)
+		for i in arr:
+			_, a, b, c, d = i
+			print(a, b, c, d,file = f)
