4c7e39b01fe14355000d1d13deae2b53e19e5b68
GCC314 <gcc_dev@163.com>
2022-03-15 20:13:31 +0800

    flush

--- test/main.go
+++ test/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/thezzisu/bltrader/common"
@@ -39,66 +38,48 @@ func ReadLine(br *bufio.Reader) (string, bool) {
 
 func main() {
 	datadir := "./data/"
-	var orders [][]common.BLOrder
-	orders = make([][]common.BLOrder, 2)
+	//var orders [][]common.BLOrder
+	//orders = make([][]common.BLOrder, 2)
 	aorder := make([]common.BLOrder, 0)
-	order_size := make([]int, 2)
+	//order_size := make([]int, 2)
 	var aosize int
 	var trades [][]common.BLTrade
 	trades = make([][]common.BLTrade, 10)
 	trade_size := make([]int, 10)
-	hooks := make([]common.BLHook, 0)
-	var hook_size int32
+	/*
+		hooks := make([]common.BLHook, 0)
+		var hook_size int32
+	*/
 	timep1 := time.Now()
 	var br *bufio.Reader
 	func() {
-		for i := 0; i < 2; i++ {
-			orders[i] = make([]common.BLOrder, 0)
-			orderFile, err := os.OpenFile(datadir+"order_"+strconv.FormatInt(int64(i+1), 10), os.O_RDONLY, 0777)
-			if err != nil {
-				panic("Failed to read order file")
-			}
-			br = bufio.NewReader(orderFile)
-			line, end := ReadLine(br)
+		orderFile, err := os.OpenFile(datadir+"stock-3.txt", os.O_RDONLY, 0777)
+		if err != nil {
+			panic("Failed to read order file")
+		}
+		br = bufio.NewReader(orderFile)
+		line, end := ReadLine(br)
+		if end {
+			panic("Order file corrupted")
+		}
+		cnt := 0
+		for {
+			line, end = ReadLine(br)
 			if end {
-				panic("Order file corrupted")
+				break
 			}
-			_, _ = fmt.Sscanf(line, "%d", &order_size[i])
-			cnt := 0
-			for {
-				line, end = ReadLine(br)
-				if end {
-					break
-				}
-				var od common.BLOrder
-				_, _ = fmt.Sscanf(line, "%d%d%d%f%d", &od.OrderId, &od.Direction, &od.Type, &od.Price, &od.Volume)
-				orders[i] = append(orders[i], od)
-				cnt++
-			}
-			orderFile.Close()
+			var od common.BLOrder
+			var o int32
+			_, _ = fmt.Sscanf(line, "%d%d%d%d%f%d", &o, &od.OrderId, &od.Direction, &od.Type, &od.Price, &od.Volume)
+			aorder = append(aorder, od)
+			cnt++
 		}
-		aosize = order_size[0] + order_size[1]
-		ii, jj := 0, 0
-		for ii < order_size[0] && jj < order_size[1] {
-			if orders[0][ii].OrderId < orders[1][jj].OrderId {
-				aorder = append(aorder, orders[0][ii])
-				ii++
-			} else {
-				aorder = append(aorder, orders[1][jj])
-				jj++
-			}
-		}
-		for ; ii < order_size[0]; ii++ {
-			aorder = append(aorder, orders[0][ii])
-		}
-		for ; jj < order_size[1]; jj++ {
-			aorder = append(aorder, orders[1][jj])
-		}
+		aosize = len(aorder)
 	}()
 	func() {
-		for i := 0; i < 10; i++ {
+		for i := 3; i == 3; i++ {
 			trades[i] = make([]common.BLTrade, 0)
-			tradeFile, err := os.OpenFile(datadir+"trade_"+strconv.FormatInt(int64(i+1), 10), os.O_RDONLY, 0777)
+			tradeFile, err := os.OpenFile(datadir+"trade03-", os.O_RDONLY, 0777)
 			if err != nil {
 				panic("Failed to read trade file")
 			}
@@ -121,36 +102,108 @@ func main() {
 			tradeFile.Close()
 		}
 	}()
-	func() {
-		hookFile, err := os.OpenFile(datadir+"hook", os.O_RDONLY, 0777)
-		if err != nil {
-			panic("Failed to read hook file")
-		}
-		br = bufio.NewReader(hookFile)
-		line, end := ReadLine(br)
-		if end {
-			panic("Hook file corrupted")
-		}
-		_, _ = fmt.Sscanf(line, "%d", &hook_size)
-		for {
-			line, end = ReadLine(br)
+	/*
+		func() {
+			for i := 0; i < 2; i++ {
+				orders[i] = make([]common.BLOrder, 0)
+				orderFile, err := os.OpenFile(datadir+"order_"+strconv.FormatInt(int64(i+1), 10), os.O_RDONLY, 0777)
+				if err != nil {
+					panic("Failed to read order file")
+				}
+				br = bufio.NewReader(orderFile)
+				line, end := ReadLine(br)
+				if end {
+					panic("Order file corrupted")
+				}
+				_, _ = fmt.Sscanf(line, "%d", &order_size[i])
+				cnt := 0
+				for {
+					line, end = ReadLine(br)
+					if end {
+						break
+					}
+					var od common.BLOrder
+					_, _ = fmt.Sscanf(line, "%d%d%d%f%d", &od.OrderId, &od.Direction, &od.Type, &od.Price, &od.Volume)
+					orders[i] = append(orders[i], od)
+					cnt++
+				}
+				orderFile.Close()
+			}
+			aosize = order_size[0] + order_size[1]
+			ii, jj := 0, 0
+			for ii < order_size[0] && jj < order_size[1] {
+				if orders[0][ii].OrderId < orders[1][jj].OrderId {
+					aorder = append(aorder, orders[0][ii])
+					ii++
+				} else {
+					aorder = append(aorder, orders[1][jj])
+					jj++
+				}
+			}
+			for ; ii < order_size[0]; ii++ {
+				aorder = append(aorder, orders[0][ii])
+			}
+			for ; jj < order_size[1]; jj++ {
+				aorder = append(aorder, orders[1][jj])
+			}
+		}()
+		func() {
+			for i := 0; i < 10; i++ {
+				trades[i] = make([]common.BLTrade, 0)
+				tradeFile, err := os.OpenFile(datadir+"trade_"+strconv.FormatInt(int64(i+1), 10), os.O_RDONLY, 0777)
+				if err != nil {
+					panic("Failed to read trade file")
+				}
+				br = bufio.NewReader(tradeFile)
+				line, end := ReadLine(br)
+				if end {
+					panic("Trade file corrupted")
+				}
+				_, _ = fmt.Sscanf(line, "%d", &trade_size[i])
+				for {
+					line, end = ReadLine(br)
+					if end {
+						break
+					}
+					var tr common.BLTrade
+					_, _ = fmt.Sscanf(line, "%d%d%f%d", &tr.BidId, &tr.AskId, &tr.Price, &tr.Volume)
+					trades[i] = append(trades[i], tr)
+				}
+				trade_size[i] = len(trades[i])
+				tradeFile.Close()
+			}
+		}()
+		func() {
+			hookFile, err := os.OpenFile(datadir+"hook", os.O_RDONLY, 0777)
+			if err != nil {
+				panic("Failed to read hook file")
+			}
+			br = bufio.NewReader(hookFile)
+			line, end := ReadLine(br)
 			if end {
-				break
+				panic("Hook file corrupted")
 			}
-			var hk common.BLHook
-			_, _ = fmt.Sscanf(line, "%d%d%d%d", &hk.SelfOrderId, &hk.TargetStkCode, &hk.TargetTradeIdx, &hk.Arg)
-			hooks = append(hooks, hk)
-		}
-		hookFile.Close()
-	}()
-	hooked := make(map[int32]bool)
-	func() {
-		for _, hook := range hooks {
-			if trades[hook.TargetStkCode-1][hook.TargetTradeIdx-1].Volume > hook.Arg {
-				hooked[hook.SelfOrderId] = true
+			_, _ = fmt.Sscanf(line, "%d", &hook_size)
+			for {
+				line, end = ReadLine(br)
+				if end {
+					break
+				}
+				var hk common.BLHook
+				_, _ = fmt.Sscanf(line, "%d%d%d%d", &hk.SelfOrderId, &hk.TargetStkCode, &hk.TargetTradeIdx, &hk.Arg)
+				hooks = append(hooks, hk)
 			}
-		}
-	}()
+			hookFile.Close()
+		}()
+		hooked := make(map[int32]bool)
+		func() {
+			for _, hook := range hooks {
+				if trades[hook.TargetStkCode-1][hook.TargetTradeIdx-1].Volume > hook.Arg {
+					hooked[hook.SelfOrderId] = true
+				}
+			}
+		}()
+	*/
 	timep2 := time.Now()
 	position := flag.Int("pos", 0, "The position of orders to start.")
 	isdump := flag.Bool("dump", false, "Set isdump as true to activate dump at the given position.")
@@ -172,14 +225,14 @@ func main() {
 					fmt.Printf("[Dump] Succeeded in Dump,the trade offset = %d\n", len(result))
 					blr.Dump()
 				}
-				if _, ok := hooked[aorder[i].OrderId]; ok {
+				if aorder[i].Volume == 0 {
 					continue
 				}
 				result = append(result, blr.Dispatch(&aorder[i])...)
 			}
 		} else {
 			for i := *position; i < aosize; i++ {
-				if _, ok := hooked[aorder[i].OrderId]; ok {
+				if aorder[i].Volume == 0 {
 					continue
 				}
 				result = append(result, blr.Dispatch(&aorder[i])...)
@@ -220,8 +273,14 @@ func main() {
 				break
 			}
 		}
+
 		if i == tnn {
 			fmt.Println("All Correct!")
+		} else {
+			fmt.Println("Wrong")
+			fmt.Println(result[j].String())
+			for j := 0; j < len(result); j++ {
+			}
 		}
 	}()
 	timep4 := time.Now()
